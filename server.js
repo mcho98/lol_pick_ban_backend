776c617ce32948979b9d9baf267810b70aaeffd4
Matthew Cho <132420015+mcho98@users.noreply.github.com>
2025-12-12 13:17:27 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+const PYTHON_URL = process.env.PYTHON_URL;
+
+
+
 
 // Forward prediction request to FastAPI model server
 app.post("/api/predict", async (req, res) => {
   try {
-    const response = await axios.post("http://localhost:8000/predict", req.body);
+    const response = await axios.post(`${PYTHON_URL}/predict`, req.body);;
     res.json(response.data);
   } catch (err) {
     console.error("Prediction error:", err.toString());
